db4eb324b294ec2ae89c02c44ceb24161297b423
22fariz22 <fariz08@gmail.com>
2023-06-10 14:49:57 +0300

    add test

--- internal/url/delivery/grpc/handler_test.go
+++ internal/url/delivery/grpc/handler_test.go
@@ -1,9 +1,71 @@
 package grpc
 
 import (
+	"context"
+	"github.com/22Fariz22/urlcutter/internal/config"
+	"github.com/22Fariz22/urlcutter/internal/url/mock"
+	urlcutter "github.com/22Fariz22/urlcutter/proto"
+	"github.com/golang/mock/gomock"
+	gonanoid "github.com/matoous/go-nanoid"
+	"github.com/stretchr/testify/require"
 	"testing"
 )
 
 func Test_service_Post(t *testing.T) {
+	t.Parallel()
 
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	UC := mock.NewMockUseCase(ctrl)
+	cfg := &config.Config{}
+	serverGRPC := NewServerGRPC(nil, cfg, UC)
+
+	//reqValue := urlcutter.ShortURL{ShortURL: "mock_short"}
+
+	t.Run("Post", func(t *testing.T) {
+		t.Parallel()
+		//respValue := urlcutter.ShortURL{ShortURL: "mock_short"}
+
+		ctx := context.Background()
+
+		id, err := gonanoid.Nanoid(10)
+		require.NoError(t, err)
+
+		reqValue := urlcutter.ShortURL{ShortURL: cfg.BaseURL + "/" + id}
+
+		UC.EXPECT().Save(ctx, gomock.Any(), gomock.Any()).Return(cfg.BaseURL+"/"+id, nil)
+
+		response, err := serverGRPC.Post(ctx, &urlcutter.LongURL{LongURL: "mock_long"})
+		require.NoError(t, err)
+		require.NotNil(t, response)
+		require.Equal(t, reqValue, reqValue)
+	})
+
+}
+
+func Test_service_Get(t *testing.T) {
+	t.Parallel()
+
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	UC := mock.NewMockUseCase(ctrl)
+	cfg := &config.Config{}
+	serverGRPC := NewServerGRPC(nil, cfg, UC)
+
+	t.Run("Get", func(t *testing.T) {
+		t.Parallel()
+
+		ctx := context.Background()
+
+		reqValue := urlcutter.LongURL{LongURL: "mock_long_url"}
+
+		UC.EXPECT().Get(ctx, gomock.Any()).Return("", nil)
+
+		response, err := serverGRPC.Get(ctx, &urlcutter.ShortURL{ShortURL: "mock_short_url"})
+		require.NoError(t, err)
+		require.NotNil(t, response)
+		require.Equal(t, reqValue, reqValue)
+	})
 }
---
+++ internal/url/repository/inmemory_test.go
@@ -0,0 +1,110 @@
+package repository
+
+import (
+	"context"
+	"sync"
+	"testing"
+)
+
+func Test_memoryStorage_Save(t *testing.T) {
+	type fields struct {
+		storage map[string]string
+		mutex   sync.RWMutex
+	}
+	newMem := NewMemory()
+
+	type args struct {
+		ctx   context.Context
+		long  string
+		short string
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		want    string
+		wantErr bool
+	}{
+		{
+			name: "ok save",
+			fields: fields{
+				storage: newMem.storage,
+				mutex:   newMem.mutex,
+			},
+			args: args{
+				ctx:   context.Background(),
+				long:  "mock_long",
+				short: "",
+			},
+			want:    "",
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			m := &memoryStorage{
+				storage: tt.fields.storage,
+				mutex:   tt.fields.mutex,
+			}
+			got, err := m.Save(tt.args.ctx, tt.args.long, tt.args.short)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("Save() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if got != tt.want {
+				t.Errorf("Save() got = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_memoryStorage_Get(t *testing.T) {
+	type fields struct {
+		storage map[string]string
+		mutex   sync.RWMutex
+	}
+
+	newMem := NewMemory()
+
+	type args struct {
+		ctx   context.Context
+		short string
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		want    string
+		wantErr bool
+	}{
+		{
+			name: "ok get",
+			fields: fields{
+				storage: newMem.storage,
+				mutex:   newMem.mutex,
+			},
+			args: args{
+				ctx:   context.Background(),
+				short: "mock_short",
+			},
+			want:    "",
+			wantErr: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			m := &memoryStorage{
+				storage: tt.fields.storage,
+				mutex:   tt.fields.mutex,
+			}
+			got, err := m.Get(tt.args.ctx, tt.args.short)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("Get() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if got != tt.want {
+				t.Errorf("Get() got = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
